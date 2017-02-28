56e2efe7c7b166ec1105ebbfb9a7d4e55a9c3c00
popenc <i.nickpope@gmail.com>
2017-02-27 19:52:18 -0500

    added 'getTransProducts' service to cts_nodejs function that sends requests to celery workers

--- node_server.js
+++ node_server.js
@@ -196,36 +196,35 @@ function parseRequestsToCeleryWorkers(sessionid, data_obj, client) {
 
 function pchemRequestHandler(sessionid, data_obj, client) {
 
-    if (data_obj['service'] == 'getSpeciationData') {
+    if (data_obj['service'] == 'getSpeciationData' || data_obj['service'] == 'getTransProducts') {
         data_obj['sessionid'] = sessionid;
         client.call('tasks.chemaxonTask', [data_obj]);
         return sessionid;
     }
+    else {
 
-    for (var calc in data_obj['pchem_request']) {
-
-        data_obj['calc'] = calc;
-        data_obj['props'] = data_obj['pchem_request'][calc];
-        data_obj['sessionid'] = sessionid;
-
-        if (calc == 'chemaxon') {
-            client.call('tasks.chemaxonTask', [data_obj]);
+        for (var calc in data_obj['pchem_request']) {
+            data_obj['calc'] = calc;
+            data_obj['props'] = data_obj['pchem_request'][calc];
+            data_obj['sessionid'] = sessionid;
+            if (calc == 'chemaxon') {
+                client.call('tasks.chemaxonTask', [data_obj]);
+            }
+            else if (calc == 'sparc') {
+                client.call('tasks.sparcTask', [data_obj]);   
+            }
+            else if (calc == 'epi') {
+                client.call('tasks.epiTask', [data_obj]);   
+            }
+            else if (calc == 'test') {
+                client.call('tasks.testTask', [data_obj]);   
+            }
+            else if (calc == 'measured') {
+                client.call('tasks.measuredTask', [data_obj]);   
+            }
         }
-        else if (calc == 'sparc') {
-            client.call('tasks.sparcTask', [data_obj]);   
-        }
-        else if (calc == 'epi') {
-            client.call('tasks.epiTask', [data_obj]);   
-        }
-        else if (calc == 'test') {
-            client.call('tasks.testTask', [data_obj]);   
-        }
-        else if (calc == 'measured') {
-            client.call('tasks.measuredTask', [data_obj]);   
-        }
+        return sessionid;
 
     }
-
-    return sessionid;  // session ID used for job ID
 
 }
