78e00b00a065b36dc8b0acc794685c9dfe315036
popenc <i.nickpope@gmail.com>
2017-10-25 21:04:19 -0400

    tidying up

--- node_server.js
+++ node_server.js
@@ -22,39 +22,18 @@ var celery = require('node-celery'),
     client = celery.createClient({
         CELERY_BROKER_URL: redis_url,
         CELERY_RESULT_BACKEND: redis_url,
-        CELERY_ROUTES: {
-            'tasks.removeUserJobsFromQueue': {
-                queue: 'manager'
-            },
-            'tasks.chemaxonTask': {
-                queue: 'chemaxon'
-            },
-            'tasks.sparcTask': {
-                queue: 'sparc'
-            },
-            'tasks.epiTask': {
-                queue: 'epi'
-            },
-            'tasks.testTask': {
-                queue: 'test'
-            },
-            'tasks.testWSTask': {
-                queue: 'test'  // putting testws on test queue
-            },
-            'tasks.measuredTask': {
-                queue: 'measured'
-            },
-            'tasks.metabolizerTask': {
-                queue: 'metabolizer'
-            },
-            'tasks.chemInfoTask': {
-                queue: 'cheminfo'
-            }
-        //     'tasks.calcTask': {
-        //         queue: 'chemaxon'
-        //     }
-        }
-    });
+    }),
+    // mapping cts_task w/ proper queue/worker:
+    celery_tester = client.createTask('tasks.test_celery', {'queue': 'manager'});
+    chemaxon_work = client.createTask('tasks.cts_task', {'queue': 'chemaxon'});
+    metabolizer_work = client.createTask('tasks.cts_task', {'queue': 'metabolizer'});
+    epi_work = client.createTask('tasks.cts_task', {'queue': 'epi'});
+    measured_work = client.createTask('tasks.cts_task', {'queue': 'measured'});
+    sparc_work = client.createTask('tasks.cts_task', {'queue': 'sparc'});
+    test_work = client.createTask('tasks.cts_task', {'queue': 'test'});
+    cheminfo_work = client.createTask('tasks.cts_task', {'queue': 'cheminfo'});
+    manager_work = client.createTask('tasks.removeUserJobsFromQueue', {'queue': 'manager'});
+
     client.on('error', function(err) {
         console.log(err);
     });
@@ -64,31 +43,25 @@ var app = express()
     , server = http.createServer(app)
     , io = io.listen(server);
 
-// var server_
-
 var nodejs_port = config.server.port;
 var nodejs_host = config.server.host;
 
 server.listen(nodejs_port);
 
 app.get('/test', function(req, res){
-  // res.send('hey');
+  // opens test page for nodejs->celery connection
   res.sendFile(path.join(__dirname + '/public/html/ws_test_page.html'));
 });
-
-// var io = require('socket.io').listen(app.listen(process.env.port));
 
 console.log("cts_nodejs running at " + nodejs_host + ", port " + nodejs_port);
 console.log("cts_nodejs test at /test");
 
-// v0.12 way:
+
 io.sockets.on('connection', function (socket) 
-{// v4+ way:
-// io.on('connection', function (socket) {
+{
 
     console.log("session id: " + socket.id);
 
-    // ??? was this a thing when it was all working???
     var message_client = redis.createClient(redis_url);
     // var message_client = redis.createClient();
     // console.log("nodejs connected to redis..");
@@ -98,16 +71,15 @@ io.sockets.on('connection', function (socket)
     
     // Grab message from Redis that was created by django and send to client
     message_client.on('message', function(channel, message){
-        // console.log("reading message from redis on channel: " + channel);
-        console.log("messaged received from django-cts via redis sub")
+        console.log("messaged received from celery worker via redis sub..")
         socket.send(message); // send to browser
     });
     
-    // checked calcs/props sent from front-end:
+    
     socket.on('get_data', function (message) {
+        // Request event from CTS Frontend
 
         console.log("nodejs server received message..");
-        // console.log(message);
 
         var message_obj = JSON.parse(message);  // parse json str to obj
 
@@ -128,12 +100,12 @@ io.sockets.on('connection', function (socket)
             message: JSON.stringify(values)
         });
 
-        // passRequestToCTS(query);
-        parseRequestsToCeleryWorkers(socket.id, message_obj, client);
+        parseCTSRequestToCeleryWorkers(socket.id, message_obj, client);  // here we go...
 
     });
 
     socket.on('disconnect', function () {
+
         console.log("user " + socket.id + " disconnected..");
         message_client.unsubscribe(socket.id); // unsubscribe from redis channel
         var message_obj = {'cancel': true};  // cancel user's jobs
@@ -143,8 +115,10 @@ io.sockets.on('connection', function (socket)
             message: JSON.stringify(message_obj)
         });
 
-        // passRequestToCTS(query);
-        parseRequestsToCeleryWorkers(socket.id, message_obj, client);
+        console.log("Calling manager worker to cancel user job upon disconnect..");
+        manager_work.call([socket.id]);
+
+        return;
 
     });
 
@@ -177,125 +151,52 @@ io.sockets.on('connection', function (socket)
 });
 
 
-function parseRequestsToCeleryWorkers(sessionid, data_obj, client) {
+function parseCTSRequestToCeleryWorkers(sessionid, data_obj, client) {
+    // TODO: Change name to old func name once it's working!
+
+    data_obj['sessionid'] = sessionid;  // add sessionid to data object
+    calc = data_obj['calc'];
 
     if ('cancel' in data_obj) {
-        client.call('tasks.removeUserJobsFromQueue', [sessionid]);
+        manager_work.call([sessionid]);
         // could send cancel notification to user..
         console.log("Calling manager worker to cancel user job upon disconnect...");
         return;
     }
 
-    var user_jobs = [];
-
-    console.log("DATA OBJ: " + JSON.stringify(data_obj));
-
-    if ('nodes' in data_obj) {
-        for (node in data_obj['nodes']) {
-            var node_obj = data_obj['nodes'][node];
-            data_obj['node'] = node_obj;
-            data_obj['chemical'] = node_obj['smiles'];
-            data_obj['mass'] = node_obj['mass'];
-            jobID = requestHandler(sessionid, data_obj, client);
-        }
-    }
-    else {
-        jobID = requestHandler(sessionid, data_obj, client);
-    }
-    
-}
-
-
-function requestHandler(sessionid, data_obj, client) {
-
-    data_obj['sessionid'] = sessionid;
-
-    console.log(">>> Service: " + data_obj['service']);
-
     if (data_obj['service'] == 'getSpeciationData') {
-        // chemspec batch and gentrans batch services
-        // data_obj['sessionid'] = sessionid;
-        client.call('tasks.chemaxonTask', [data_obj]);
-        // client.call('tasks.calcTask', [data_obj]);
-        return sessionid;
+        console.log("calling chemaxon worker for speciation data..");
+        chemaxon_work.call([data_obj]);
     }
     else if (data_obj['service'] == 'getTransProducts') {
         console.log("calling metabolizer worker for transformation products");
-        client.call('tasks.metabolizerTask', [data_obj]);
-        return sessionid;
+        metabolizer_work.call([data_obj]);
     }
     else if (data_obj['service'] == 'getChemInfo') {
         console.log("calling chem info worker..");
-        client.call('tasks.chemInfoTask', [data_obj]);
-        return sessionid;
+        cheminfo_work.call([data_obj]);
     }
     else {
-
-        callPchemWorkers(sessionid, data_obj, client);  // sends requests to pchem workers
-        return sessionid;
-
-    }
-
-}
-
-
-function callPchemWorkers(sessionid, data_obj, client) {
-
-    console.log("calling pchem worker");
-    console.log("data obj: " + data_obj);
-
-    for (var calc in data_obj['pchem_request']) {
-
-        var props = data_obj['pchem_request'][calc];
-
-        data_obj['calc'] = calc;
-        // data_obj['props'] = data_obj['pchem_request'][calc];
-        // data_obj['sessionid'] = sessionid;
-
-        for (var prop_index = 0; prop_index < props.length; prop_index++) {
-
-            var prop = props[prop_index];
-
-            console.log("props " + props);
-            console.log("prop " + prop);
-
-            data_obj['prop'] = prop;
-
-            var is_chemaxon = calc == 'chemaxon';
-            var is_kow = prop == 'kow_no_ph' || prop == 'kow_wph';
-            if (is_chemaxon && is_kow) {
-                // chemaxon kow has values for 3 different methods
-                // note: this'll be in consumer.py and use the calc classes,
-                // so it can grab the methods from there..
-                for (var i = 0; i < chemaxon_kow_methods.length; i++) {
-                    data_obj['method'] = chemaxon_kow_methods[i];
-                    console.log("CHEMAXON KOW METHOD: " + chemaxon_kow_methods[i]);
-                    client.call('tasks.chemaxonTask', [data_obj]);
-                }
+        // Breaking user request up by calc, send to
+        // the respective calc celery workers:
+        for (var calc in data_obj['pchem_request']) {
+            data_obj['calc'] = calc;
+            if (calc == 'chemaxon') {
+                chemaxon_work.call([data_obj]); // this it??
             }
-            else {
-
-                if (calc == 'chemaxon') {
-                    console.log("sending request to chemaxon task");
-                    client.call('tasks.chemaxonTask', [data_obj]);
-                }
-                else if (calc == 'sparc') {
-                    client.call('tasks.sparcTask', [data_obj]);
-                }
-                else if (calc == 'epi') {
-                    client.call('tasks.epiTask', [data_obj]);   
-                }
-                else if (calc == 'test') {
-                    client.call('tasks.testTask', [data_obj]);   
-                }
-                else if (calc == 'testws') {
-                    console.log("sending request to testws task");
-                    client.call('tasks.testWSTask', [data_obj]);
-                }
-                else if (calc == 'measured') {
-                    client.call('tasks.measuredTask', [data_obj]);   
-                }
+            else if (calc == 'sparc') {
+                sparc_work.call([data_obj]);
+            }
+            else if (calc == 'epi') {
+                epi_work.call([data_obj]);
+            }
+            else if (calc == 'test') {
+                test_work.call([data_obj]);
+            }
+            else if (calc == 'measured') {
+                measured_work.call([data_obj]);  
             }
         }
     }
+    
 }
